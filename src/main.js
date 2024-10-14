9490bc4bf6966e62b4613e91a9cd912a88cb196d
Kimmyli <kc.szul@gmail.com>
2024-10-14 17:54:48 +0200

    VITE WORKS

--- src/main.js
+++ src/main.js
@@ -3,8 +3,6 @@ import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
 import { VRButton } from 'three/addons/webxr/VRButton.js';
 
-console.log("Script is running...");
-
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -15,11 +13,11 @@ const camera = new THREE.PerspectiveCamera(
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.setClearColor(new THREE.Color(0xe1e1ea))
+renderer.setClearColor(new THREE.Color(0xe1e1ea));
 renderer.setAnimationLoop(animate);
-document.body.appendChild(renderer.domElement);
-
 document.body.appendChild( VRButton.createButton( renderer ) );
+
+document.body.appendChild(renderer.domElement);
 renderer.xr.enabled = true;
 
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1.4);
@@ -27,7 +25,7 @@ directionalLight.position.set(0, 10, 10);
 scene.add(directionalLight);
 
 const directionalLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
-scene.add(directionalLightHelper)
+scene.add(directionalLightHelper);
 
 const loader = new GLTFLoader();
 loader.load(
@@ -42,8 +40,14 @@ loader.load(
 );
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enablePan = false
-controls.mouseButtons.RIGHT = THREE.MOUSE.ROTATE;
+controls.mouseButtons.RIGHT = false;
+controls.listenToKeyEvents(window);
+controls.keys = {
+	LEFT: 'KeyA',
+	UP: 'KeyW', 
+	RIGHT: 'KeyD',
+	BOTTOM: 'KeyS'
+};
 camera.position.set(-50, 4, 0 );
 
 function animate() {
