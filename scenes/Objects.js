dbc9562e2d760c8bb72c6fcba8e74ea6f52c8617
Kimmyli <kc.szul@gmail.com>
2025-01-08 10:02:43 +0100

    S P A C E

--- scenes/Objects.js
+++ scenes/Objects.js
@@ -20,7 +20,7 @@ export function createScene1(scene, camera, renderer, gui, fileInput, controls) 
   const loadedObjects = {}; // To store loaded objects
   let currentObject = null; // Track currently displayed object
 
-    fileInput.accept = '.obj, .glb, . fbx, .stl'
+    fileInput.accept = '.obj, .glb, .fbx, .stl'
     fileInput.addEventListener('change', async (event) => {
         const file = event.target.files[0];
         if (file) {
