756a59e6f66b97299f59b5b6499fef8624260a38
lars <koninglars04@gmail.com>
2025-01-15 09:54:21 +0100

    Update Panoramas.js

--- scenes/Panoramas.js
+++ scenes/Panoramas.js
@@ -3,16 +3,22 @@ import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 
 export function createScene2(scene, camera, renderer, gui, fileInput) {
-  const filesList = [];
+  const filesList = new Set();
+  let currentImage = null;
+  let currentCanvas = document.createElement("canvas");
+  let currentContext = currentCanvas.getContext("2d");
   let currentFile = null;
-  let sphereMesh = null;
+
+  // Google API credentials
+  const API_KEY = "AIzaSyBSRS_xgFTJ7g2g26ilFw1jgmzhpCYA1M4";
+  const PAN_FOLDER_ID = "1tG8uMunbeUYeRWwIv8hQAst7MiOk1mvx";
 
   // Voeg een bestand toe aan de lijst en toon het in de mediaviewer
-  async function addFile(file) {
-    const fileURL = URL.createObjectURL(file);
-    filesList.push({ name: file.name, url: fileURL });
-    updateMediaViewer();
-  }
+  // async function addFile(file) {
+  //   const fileURL = URL.createObjectURL(file);
+  //   filesList.push({ name: file.name, url: fileURL });
+  //   updateMediaViewer();
+  // }
 
   function updateMediaViewer() {
     if (!mediaViewerFolder) {
@@ -66,39 +72,217 @@ export function createScene2(scene, camera, renderer, gui, fileInput) {
   }
 
   // Bestand uploaden via de fileInput
-  fileInput.accept = "image/jpeg, image/png, image/webp";
-  fileInput.addEventListener("change", async (event) => {
-    const file = event.target.files[0];
-    if (file) {
-      const fileExtension = file.name
-        .slice(file.name.lastIndexOf("."))
-        .toLowerCase();
-      if ([".jpg", ".jpeg", ".png", ".webp"].includes(fileExtension)) {
-        console.log(`File uploaded: ${file.name}`);
-        await addFile(file);
-      } else {
-        alert(
-          "Invalid file type. Please upload a .jpg, .jpeg, .png, or .webp file."
-        );
-      }
-    }
-  });
+  // fileInput.accept = "image/jpeg, image/png, image/webp";
+  // fileInput.addEventListener("change", async (event) => {
+  //   const file = event.target.files[0];
+  //   if (file) {
+  //     const fileExtension = file.name
+  //       .slice(file.name.lastIndexOf("."))
+  //       .toLowerCase();
+  //     if ([".jpg", ".jpeg", ".png", ".webp"].includes(fileExtension)) {
+  //       console.log(`File uploaded: ${file.name}`);
+  //       await addFile(file);
+  //     } else {
+  //       alert(
+  //         "Invalid file type. Please upload a .jpg, .jpeg, .png, or .webp file."
+  //       );
+  //     }
+  //   }
+  // });
+
+  fetchPhotosFromDrive();
 
   // GUI instellingen
   const settings = {
-    upload: function () {
-      fileInput.click();
-    },
+    exposure: 0,
+    highlights: 0,
+    shadows: 0,
   };
 
-  gui.add(settings, "upload").name("Upload Bestand");
+  const editing = gui.addFolder("Bewerkings opties");
+  editing
+    .add(settings, "exposure", -100, 100)
+    .name("Helderheid")
+    .onChange(applyAdjustments);
+  editing
+    .add(settings, "highlights", -100, 100)
+    .name("Highlights")
+    .onChange(applyAdjustments);
+  editing
+    .add(settings, "shadows", -100, 100)
+    .name("Schaduwen")
+    .onChange(applyAdjustments);
+
+  // gui.add(settings, "upload").name("Upload Bestand");
 
   var mediaViewerFolder = gui.addFolder("Galerij");
+
   const folderElement = mediaViewerFolder.domElement;
+  folderElement.classList.add("mediaFolder");
   folderElement.id = "mediaFolder";
 
+  async function fetchPhotosFromDrive() {
+    try {
+      const response = await fetch(
+        `https://www.googleapis.com/drive/v3/files?q='${PAN_FOLDER_ID}'+in+parents+and+mimeType+contains+'image/'&key=${API_KEY}&fields=files(id,name,mimeType)`
+      );
+
+      const data = await response.json();
+      console.log("Photos data:", data);
+      const files = data.files;
+
+      if (files && files.length > 0) {
+        files.forEach((file) => {
+          if (!filesList.has(file.id)) {
+            filesList.add(file.id);
+            file.url = `https://www.googleapis.com/drive/v3/files/${file.id}?alt=media&key=${API_KEY}`;
+
+            if (!mediaViewerFolder) {
+              mediaViewerFolder = gui.addFolder("Photo's");
+              const folderElement = mediaViewerFolder.domElement;
+              folderElement.classList.add("photosFolder");
+              folderElement.id = "photosFolder";
+            }
+
+            const index = filesList.size - 1;
+            const propertyName = `Foto ${index + 1}`;
+            const elementController = mediaViewerFolder.add(
+              { [propertyName]: () => {} },
+              propertyName
+            );
+
+            elementController.name(file.name);
+
+            // Stel de achtergrond van de widget in als thumbnail
+            const buttonElement = elementController.domElement;
+            buttonElement.style.cursor = "pointer";
+            buttonElement.style.backgroundImage = `url(${file.url})`;
+            buttonElement.style.backgroundSize = "cover";
+            buttonElement.style.backgroundPosition = "center";
+            buttonElement.style.border = "2px solid transparent";
+
+            buttonElement.addEventListener("click", () => {
+              currentFile = file;
+              applyAdjustments();
+              // reset the canvas
+              while (scene.children.length > 0) {
+                const object = scene.children[0];
+                if (object.geometry) object.geometry.dispose();
+                if (object.material) {
+                  if (object.material.map) object.material.map.dispose();
+                  object.material.dispose();
+                }
+                scene.remove(object);
+              }
+
+              const allButtons = document.querySelectorAll(
+                "#photosFolder button"
+              );
+              allButtons.forEach(
+                (btn) => (btn.style.border = "2px solid transparent")
+              );
+              buttonElement.style.border = "2px solid #00f";
+
+              const img = new Image();
+              img.onload = () => {
+                const width = img.width / 2;
+                const height = img.height / 2;
+
+                const textureLoader = new THREE.TextureLoader();
+                textureLoader.load(
+                  file.url,
+                  (texture) => {
+                    const material = new THREE.MeshBasicMaterial({
+                      map: texture,
+                      side: THREE.DoubleSide,
+                    });
+
+                    const geometry = new THREE.PlaneGeometry(
+                      width / 100,
+                      height / 100
+                    );
+                    currentImage = new THREE.Mesh(geometry, material);
+                    currentImage.position.set(0, 0, -5);
+                    scene.add(currentImage);
+                  },
+                  undefined,
+                  (err) => console.error("Texture loading error:", err)
+                );
+              };
+              img.src = file.url;
+            });
+          }
+        });
+      } else {
+        console.log("No photos found in the specified folder.");
+      }
+    } catch (error) {
+      console.error("Error fetching photos from Google Drive:", error);
+    }
+  }
+
+  function applyAdjustments() {
+    if (!currentFile || !currentContext) return;
+
+    const img = new Image();
+    img.onload = () => {
+      currentCanvas.width = img.width;
+      currentCanvas.height = img.height;
+      currentContext.drawImage(img, 0, 0);
+
+      const imgData = currentContext.getImageData(
+        0,
+        0,
+        currentCanvas.width,
+        currentCanvas.height
+      );
+      const data = imgData.data;
+
+      const exposureFactor = Math.pow(2, settings.exposure / 100);
+      const highlightsFactor = settings.highlights / 100;
+      const shadowsFactor = settings.shadows / 100;
+
+      for (let i = 0; i < data.length; i += 4) {
+        data[i] = adjustPixel(
+          data[i],
+          exposureFactor,
+          highlightsFactor,
+          shadowsFactor
+        ); // R
+        data[i + 1] = adjustPixel(
+          data[i + 1],
+          exposureFactor,
+          highlightsFactor,
+          shadowsFactor
+        ); // G
+        data[i + 2] = adjustPixel(
+          data[i + 2],
+          exposureFactor,
+          highlightsFactor,
+          shadowsFactor
+        ); // B
+      }
+
+      currentContext.putImageData(imgData, 0, 0);
+      displayImageInScene(currentCanvas.toDataURL());
+    };
+
+    img.src = currentFile.url;
+  }
+
   function displayPanoramaInScene(fileURL) {
-    resetScene();
+    // Reset de scene
+    while (scene.children.length > 0) {
+      const object = scene.children[0];
+      if (object.geometry) object.geometry.dispose();
+      if (object.material) {
+        if (object.material.map) object.material.map.dispose();
+        object.material.dispose();
+      }
+      scene.remove(object);
+    }
+
+    sphereMesh = null;
 
     const loader = new THREE.ImageLoader();
     const loadingIndicator = createLoadingIndicator();
@@ -135,29 +319,29 @@ export function createScene2(scene, camera, renderer, gui, fileInput) {
     );
   }
 
-  function validatePanorama(image) {
-    if (image.width / image.height !== 2) {
-      alert(
-        "Dit bestand lijkt geen geldig panorama te zijn. Breedte/hoogte verhouding moet 2:1 zijn."
-      );
-      return false;
-    }
-    return true;
-  }
+  // function validatePanorama(image) {
+  //   if (image.width / image.height !== 2) {
+  //     alert(
+  //       "Dit bestand lijkt geen geldig panorama te zijn. Breedte/hoogte verhouding moet 2:1 zijn."
+  //     );
+  //     return false;
+  //   }
+  //   return true;
+  // }
 
-  function resetScene() {
-    while (scene.children.length > 0) {
-      const object = scene.children[0];
-      if (object.geometry) object.geometry.dispose();
-      if (object.material) {
-        if (object.material.map) object.material.map.dispose();
-        object.material.dispose();
-      }
-      scene.remove(object);
-    }
+  // function resetScene() {
+  //   while (scene.children.length > 0) {
+  //     const object = scene.children[0];
+  //     if (object.geometry) object.geometry.dispose();
+  //     if (object.material) {
+  //       if (object.material.map) object.material.map.dispose();
+  //       object.material.dispose();
+  //     }
+  //     scene.remove(object);
+  //   }
 
-    sphereMesh = null;
-  }
+  //   sphereMesh = null;
+  // }
 
   function createLoadingIndicator() {
     const loadingIndicator = document.createElement("div");
